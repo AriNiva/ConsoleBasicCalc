3d9a3462973c3adedb27cfa9382bf6ed469f4a54
AriNiva <145963185+AriNiva@users.noreply.github.com>
2023-09-25 10:35:33 +0300

    Add files via upload

---
+++ ConsoleBasicCalc/ConsoleBasicCalc/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleBasicCalc
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Double dSumma = 0;
+            string strSumma = "";
+            string luku = "0";
+            while (luku.Length > 0)//Length on stringin pituus.5=1 10=2jne.
+            { 
+                dSumma = dSumma + Double.Parse(luku);//Mennään silmukkaan, ensimmäisellä kierroksella intSumma + luku = 0+0
+                Console.Write("Anna luku ({0}): ", dSumma.ToString());//({0}) viittaa parametriin intSumma.ToString.Ensimmäisellä kierroksella 0
+                luku = Console.ReadLine();
+            }
+            Console.WriteLine("Lukujen summa on " + dSumma.ToString());
+            Console.WriteLine();
+            Console.Write("Paina enter lopettaaksesi!");
+            Console.ReadLine();
+        }
+    }
+}
